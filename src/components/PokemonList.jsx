3af075d3e9ac8e9448ef181e6cab0234616dcc9b
The_Viga <rojas_exp@hotmail.com>
2023-10-23 16:59:41 -0500

    Completado la api de pokemon

--- src/components/PokemonList.jsx
+++ src/components/PokemonList.jsx
@@ -1,17 +1,33 @@
-import React, { useContext } from "react";
-import { PokemonContext } from '../context/PokemonContext'
-import { CardPokemon } from "./CardPokemon";
+import React, { useContext } from 'react';
+import { PokemonContext } from '../context/PokemonContext';
+import { CardPokemon } from './CardPokemon';
+import { Loader } from './Loader';
 
 export const PokemonList = () => {
+	const { allPokemons, loading, filteredPokemons } =
+		useContext(PokemonContext);
 
-    const {allPokemons} = useContext(PokemonContext)
-
-    return(
-        <div className="card-list-pokemon container">
-
-            {allPokemons.map(pokemon => (
-                <CardPokemon pokemon={pokemon} key={pokemon.id} />
-            ))}
-        </div>
-    )
-}
+	return (
+		<>
+			{loading ? (
+				<Loader />
+			) : (
+				<div className='card-list-pokemon container'>
+					{filteredPokemons.length ? (
+						<>
+							{filteredPokemons.map(pokemon => (
+								<CardPokemon pokemon={pokemon} key={pokemon.id} />
+							))}
+						</>
+					) : (
+						<>
+							{allPokemons.map(pokemon => (
+								<CardPokemon pokemon={pokemon} key={pokemon.id} />
+							))}
+						</>
+					)}
+				</div>
+			)}
+		</>
+	);
+};
